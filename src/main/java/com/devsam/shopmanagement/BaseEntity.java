b4b6412c605f6d5150dc7cfd72367e5cd5acff5e
gitongasam <gitongas684@gmail.com>
2025-11-06 05:19:01 +0300

    BaseEntity setup

--- src/main/java/com/devsam/shopmanagement/BaseEntity.java
+++ src/main/java/com/devsam/shopmanagement/BaseEntity.java
@@ -1,5 +1,32 @@
 package com.devsam.shopmanagement;
 
-public class BaseEntity {
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime deletedAt;
 }
