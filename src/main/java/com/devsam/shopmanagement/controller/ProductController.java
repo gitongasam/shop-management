34e2dabe8f301b6cf9d4018baeace87f71d04dfb
gitongasam <gitongas684@gmail.com>
2025-11-11 11:19:53 +0300

    added get product by id

--- src/main/java/com/devsam/shopmanagement/controller/ProductController.java
+++ src/main/java/com/devsam/shopmanagement/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.devsam.shopmanagement.controller;
 import com.devsam.shopmanagement.dtos.ProductRequest;
 import com.devsam.shopmanagement.entity.Product;
 import com.devsam.shopmanagement.entity.User;
+import com.devsam.shopmanagement.errors.ResourceNotFoundException;
 import com.devsam.shopmanagement.repository.UserRepository;
 import com.devsam.shopmanagement.service.product_service.ProductService;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +13,8 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("api/v1/products")
 @RequiredArgsConstructor
@@ -20,6 +23,7 @@ public class ProductController {
     private final ProductService productService;
     private final UserRepository userRepository;
 
+//    add product
     @PostMapping
     public Product AddProduct(@RequestBody ProductRequest productRequest, @AuthenticationPrincipal UserDetails userDetails) {
        String email = userDetails.getUsername();
@@ -28,6 +32,7 @@ public class ProductController {
         return productService.addProduct(productRequest, user);
     }
 
+//     get all product
     @GetMapping
     public Page<Product> getProducts(Pageable pageable, @AuthenticationPrincipal UserDetails userDetails) {
         String email = userDetails.getUsername();
@@ -35,5 +40,11 @@ public class ProductController {
                 .orElseThrow(() -> new RuntimeException("User not found"));
 
         return productService.getAllProducts(pageable, user);
+    }
+
+//    get product by id
+    @GetMapping("{id}")
+    public Product getProductById(@PathVariable UUID id) {
+        return productService.getProductById(id);
     }
 }
